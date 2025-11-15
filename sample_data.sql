381b17307847c1344193cfacb1800c93a846c377
Prabhu0711 <163495445+Prabhu0711@users.noreply.github.com>
2025-11-15 21:04:06 +0530

    Add sample data for Products, Users, Orders, and OrderItems

---
+++ sample_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO Products VALUES
+(1, 'Phone', 15000.00, 'Electronics'),
+(2, 'Shoes', 2000.00, 'Fashion'),
+(3, 'Watch', 1200.00, 'Fashion');
+
+INSERT INTO Users VALUES
+(1, 'Nikhil', 'nikhil@gmail.com'),
+(2, 'Sara', 'sara@gmail.com');
+
+INSERT INTO Orders VALUES
+(101, 1, '2024-01-05'),
+(102, 2, '2024-01-06');
+
+INSERT INTO OrderItems VALUES
+(101, 1, 1),
+(101, 3, 2),
+(102, 2, 1);
